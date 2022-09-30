21495d1baf5f8ab72e01936a780d5fa67ddf7cc4
Hassan_Ihsan <ihsanhassan.business@gmail.com>
2022-09-30 15:04:11 -0400

    Update: fixed mestake

--- CreateNewRepos.sh
+++ CreateNewRepos.sh
@@ -6,14 +6,15 @@ set -e
 printf "\033[0;32mCreating New GitHub Repositorie...\033[0m\n"
 
 #creating a new repos
-
 #create a echo for a README.md file
-README=""
 #press ESC to quit entering message
-echo "Press ESC key to quit"
 printf "\033[0;32mAdd README.md FIle Message $(date):\033[0m\n" 
+echo "Press ESC key to quit"
+echo -e "\n"
 
-# read a single characters
+README=""
+
+# README.md file message same as before
 while IFS= read -r -n1 key1
 do
 # if input == ESC key
@@ -22,20 +23,36 @@ then
 break;
 fi
 # Add the key to the variable which is pressed by the users.
-README+=$key
+README+=$key1
 done
 
 echo "$README" >> README.md
 
 # git init
 git init
- 
+echo -e "\n"
+
+
+
+#git add README.md 
 git add README.md
+echo -e "\n"
 
+
+
+#git commit -m 
 git commit -m "first commit"
+echo -e "\n"
 
+
+
+#git branch -M main 
 git branch -M main
+echo -e "\n"
 
+
+
+#this is where you will paste in the given http link by github
 remoteConnectHttp=""
 #press ESC to quit entering message
 echo "Press ESC key to quit"
@@ -54,9 +71,13 @@ fi
 remoteConnectHttp+=$key
 done
 
+#git remove add origin
 git remote add origin "$remoteConnectHttp"
+echo -e "\n"
+
 
 # Push source
 git push -u origin main
+ 
 
  
